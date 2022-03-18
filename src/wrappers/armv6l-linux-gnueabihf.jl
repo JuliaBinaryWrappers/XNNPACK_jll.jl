7644967b11d4f0ef4d8cc13cea4d2eaff0b4f690
jlbuild <juliabuildbot@gmail.com>
2022-03-18 09:56:12 +0000

    XNNPACK_jll build 0.0.20200323+0

--- src/wrappers/armv6l-linux-gnueabihf.jl
+++ src/wrappers/armv6l-linux-gnueabihf.jl
@@ -4,7 +4,7 @@ export libxnnpack
 using CPUInfo_jll
 using PThreadPool_jll
 JLLWrappers.@generate_wrapper_header("XNNPACK")
-JLLWrappers.@declare_library_product(libxnnpack, "libXNNPACK.so")
+JLLWrappers.@declare_library_product(libxnnpack, "libXNNPACK.so.0")
 function __init__()
     JLLWrappers.@generate_init_header(CPUInfo_jll, PThreadPool_jll)
     JLLWrappers.@init_library_product(
